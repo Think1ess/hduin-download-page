5a2caa15bc1e4543a494661b9959093b999bf741
Think1ess <993639686@qq.com>
2021-03-17 20:00:32 +0800

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+/**
+ * Created by lucas on 10/23/15.
+ */
+
+
+console.log("%c小伙子我看你骨骼精奇,快快加入我们吧:mailto://hr@redhome.cc", "color: #ff2600;");
+
+function redirect_iTunes() {
+    window.location = "http://itunes.apple.com/app/id1038880945";
+}
+
+function redirectGooglePlay() {
+    window.location = "https://play.google.com/store/apps/details?id=cc.redhome.hduin.android";
+}
+
